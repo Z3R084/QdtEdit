7d52a6c08ed60c98290eed846f6c0a835e58e058
Christian Quast <christian.quast84@googlemail.com>
2018-03-05 21:20:05 +0100

    added demo infrastructure

---
+++ demo/src/app/app.component.ts
@@ -0,0 +1,8 @@
+import { Router, NavigationEnd } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qdtd-app',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {}
